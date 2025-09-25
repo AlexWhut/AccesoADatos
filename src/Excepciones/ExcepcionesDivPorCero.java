527ae950cae58502d73eba3be7b946292bcbc6bb
AlexWhut <pdxetrz@gmail.com>
2025-09-25 09:44:20 +0200

    Actividad 02

---
+++ src/Excepciones/ExcepcionesDivPorCero.java
@@ -0,0 +1,12 @@
+public class Excepciones {
+        public static void main(String[] args) {
+            int a, b;
+            a = 5;
+            b = 2;
+            System.out.println(a + "/" + b + "=" + (a / b));
+            b = 0;
+            System.out.println(a + "/" + b + "=" + (a / b));
+        }
+    }
+    
+
