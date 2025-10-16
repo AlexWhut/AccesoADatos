fdb9e8938355a53f5bb53534e9ee9f0fdfbbd9b0
AlexWhut <pdxetrz@gmail.com>
2025-10-16 09:27:15 +0200

    update

--- src/EJercicios/VolcadoBinario.java
+++ src/EJercicios/VolcadoBinario.java
@@ -1,32 +1,44 @@
 package EJercicios;
 
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.PrintStream;
 
 public class VolcadoBinario {
-    public static void main(String[] args) {
-        if (args.length < 1) {
-            System.out.println("Por favor, indica el nombre del archivo binario como argumento.");
-            return;
-        }
 
-        String nombreArchivo = args[0];
-
+    // Método que hace el volcado a cualquier PrintStream
+    public static void volcarArchivo(String nombreArchivo, PrintStream salida) throws IOException {
         try (FileInputStream fis = new FileInputStream(nombreArchivo)) {
             int byteLeido;
             int contador = 0;
             while ((byteLeido = fis.read()) != -1) {
-                System.out.printf("%02X ", byteLeido);
+                salida.printf("%02X ", byteLeido);
                 contador++;
                 if (contador % 16 == 0) {
-                    System.out.println();
+                    salida.println();
                 }
             }
             if (contador % 16 != 0) {
-                System.out.println();
+                salida.println();
             }
+        }
+    }
+
+    public static void main(String[] args) {
+        if (args.length < 2) {
+            System.out.println("Uso: java EJercicios.VolcadoBinario <archivo_entrada> <archivo_salida>");
+            return;
+        }
+
+        String nombreArchivoEntrada = args[0];
+        String nombreArchivoSalida = args[1];
+
+        try (PrintStream ps = new PrintStream(new FileOutputStream(nombreArchivoSalida))) {
+            volcarArchivo(nombreArchivoEntrada, ps);
+            System.out.println("Volcado completado. Revisa el archivo: " + nombreArchivoSalida);
         } catch (IOException e) {
-            System.out.println("Ocurrió un error al leer el archivo: " + e.getMessage());
+            System.out.println("Ocurrió un error: " + e.getMessage());
         }
     }
 }
